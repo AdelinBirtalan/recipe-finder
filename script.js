014ce906d23c61071fba2628431c1dacecb23dd6
unknown <adelinbirtalan3@gmail.com>
2025-03-17 13:51:46 +0200

    Schimbari design

--- script.js
+++ script.js
@@ -11,16 +11,23 @@ const prefersDark =
   window.matchMedia("(prefers-color-scheme: dark)").matches;
 const savedTheme = localStorage.getItem("darkMode");
 
+const loader = document.getElementById("loader");
+
 myBtn.addEventListener("click", () => {
   const query = input.value.trim();
   if (query == "") {
     alert("Please enter an ingredient!");
     return;
   }
+
+  loader.classList.remove("hidden");
+  resultsDiv.innerHTML = "";
+
   const url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}&to=8`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      loader.classList.add("hidden");
       if (data.hits.length === 0) {
         resultsDiv.innerHTML =
           "<p>No recipes found. Try different ingredients!</p>";
@@ -29,6 +36,7 @@ myBtn.addEventListener("click", () => {
       displayRecipes(data.hits);
     })
     .catch((error) => {
+      loader.classList.add("hidden");
       console.error("Error fetching data:", error);
       resultsDiv.innerHTML = "<p>Something went wrong. Please try again!</p>";
     });
@@ -67,3 +75,37 @@ darkModeToggle.addEventListener("change", function () {
     localStorage.setItem("darkMode", "false");
   }
 });
+
+(function () {
+  function c() {
+    var b = a.contentDocument || a.contentWindow.document;
+    if (b) {
+      var d = b.createElement("script");
+      d.innerHTML =
+        "window.__CF$cv$params={r:'921c3966ce7eb066',t:'MTc0MjIxMTcxOC4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";
+      b.getElementsByTagName("head")[0].appendChild(d);
+    }
+  }
+  if (document.body) {
+    var a = document.createElement("iframe");
+    a.height = 1;
+    a.width = 1;
+    a.style.position = "absolute";
+    a.style.top = 0;
+    a.style.left = 0;
+    a.style.border = "none";
+    a.style.visibility = "hidden";
+    document.body.appendChild(a);
+    if ("loading" !== document.readyState) c();
+    else if (window.addEventListener)
+      document.addEventListener("DOMContentLoaded", c);
+    else {
+      var e = document.onreadystatechange || function () {};
+      document.onreadystatechange = function (b) {
+        e(b);
+        "loading" !== document.readyState &&
+          ((document.onreadystatechange = e), c());
+      };
+    }
+  }
+})();
